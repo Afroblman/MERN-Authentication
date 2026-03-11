2d3d052ea6e1879ec15007d574d597fae818ac9e
Blman <tadesseagegnehu12@gmail.com>
2026-03-11 09:51:01 +0300

    frontend url change

--- server/app.js
+++ server/app.js
@@ -11,7 +11,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mern-authentication-woad.vercel.app/",
+    ],
     credentials: true,
   }),
 );
